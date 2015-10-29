bb06f5463220b9cf5ddfde377915c920e2dc5904
XiaoChenYung <18730222889@126.com>
2015-10-29 17:32:59 +0800

    初始化

---
+++ JSONDemo/JSONDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONDemo
+//
+//  Created by 杨晓晨 on 15/10/29.
+//  Copyright © 2015年 杨晓晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONDemo/JSONDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONDemo
+//
+//  Created by 杨晓晨 on 15/10/29.
+//  Copyright © 2015年 杨晓晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
